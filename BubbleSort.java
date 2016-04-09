c9157753dcb158c5b171fa32c170cac58387dde0
Liu-Hong-De <U10416012@go.utaipei.edu.tw>
2016-04-09 10:35:52 +0800

    change the quene of the user input number
    if the first number  is big than the second number, and then change their quene, so the number can be ordered
    and then print out the result

--- BubbleSort.java
+++ BubbleSort.java
@@ -19,5 +19,21 @@ public class BubbleSort {
 		for (int c = 0; c < n; c++) {
 			array[c] = input.nextInt();
 		}
+		//change the quene of the user input number
+		for (int c = 0; c < ( n - 1 ); c++) {
+			for (int d = 0; d < n - c - 1; d++) {
+				if (array[d] > array[d + 1]) {
+					int swap = array[d];
+					array[d] = array[d + 1];
+					array[d + 1] = swap;
+				}
+			}
+		}
+ 
+		System.out.println("the answer of bubble sort is");
+ 
+		//print the answer
+		for (int c = 0; c < n; c++) 
+			System.out.println(array[c]);
 	}
 }
